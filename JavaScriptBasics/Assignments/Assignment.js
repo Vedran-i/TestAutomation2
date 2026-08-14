eaa0776f728e24bf88e0519f570abb6023bd43bd
vedran-i <vedde13@gmail.com>
2026-08-14 15:49:26 +0200

    3rd commit

---
+++ JavaScriptBasics/Assignments/Assignment.js
@@ -0,0 +1,8 @@
+//Assignments 
+
+// 1. get the sum of first 100 numbers
+
+
+
+// 2. reverse the number 3982
+// 3. reverse string - Javascript
---
+++ JavaScriptBasics/TC-04LoopingStatement.js
@@ -0,0 +1,36 @@
+//For loop
+for (let i = 0; i<5; i++) {
+console.log(i);
+
+}
+
+//for(initialization, condition, increment/decrement)
+
+//perform addition of first 10 numbers using for loop
+
+let sum = 0;
+//1+2+3+4+5 etc
+//
+for (let i = 1; i < 11; i++)
+{
+    sum = sum + i;
+}
+console.log(sum); //55
+
+//while loop
+let j = 1;
+while (j < 5){
+
+    console.log(j);
+    j++; //or j = j + 1;
+}
+
+//do while loop
+
+let k = 100;
+do{
+    console.log(k);
+    k++;
+
+} 
+while(k < 105);
