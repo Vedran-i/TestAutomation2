b63b78eec774482a953655300418b39bbb486f36
vedran-i <vedde13@gmail.com>
2026-09-11 15:12:07 +0200

    Updated

---
+++ tests/TC03HooksDemo.spec.js
@@ -0,0 +1,26 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('Hooks Demo', () => {
+//beforeEach hook will run before each test in this describe block meaning that before each test 
+// it will navigate to the baseURL which is defined in the playwright.config.js file
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+    })
+
+    test('login to OrangeHRM with valid credentials', async ({ page }) => {
+       
+        await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com');
+    })
+
+    test('validate the page title', async ({ page }) => {
+        await expect(page).toHaveTitle('OrangeHRM');
+    })
+
+    test('validate the logo of the page', async ({ page }) => {
+        await expect(page.getByAltText('company-branding')).toBeVisible();
+    });
+
+    test('validate admin tab', async ({ page }) => {
+        await expect(page.getByText('Admin')).toBeVisible();
+    });
+});
